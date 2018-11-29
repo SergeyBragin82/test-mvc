c7adecf19072373c8d8661d9f5278eaee8d10399
codcoderov <cod.coderov@gmail.com>
2018-11-29 11:10:15 +0300

    old_1.1

--- mvcweb/mvcadmin.php
+++
@@ -1,3 +0,0 @@
-<?php
-?>
-MVC Admin Goes Here
--- mvcweb/templates/general-information/generic.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	echo horizontalBreak(); 
-?>
-<div class="container general-info terms-of-use">
-	<?php
-		echo htmlspecialchars_decode($context->xpath("//page_data/.")[0]->asXML());
-	?>
-</div>
